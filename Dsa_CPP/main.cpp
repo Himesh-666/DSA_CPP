1bdb4494540fcb1949469b5d391e7ac5264f5b84
Himeshwar polisetty <himeshwarpolisetty@gmail.com>
2026-05-26 20:29:33 +0530

    Updated on 26/may/26

--- Dsa_CPP/main.cpp
+++ Dsa_CPP/main.cpp
@@ -1,14 +1,143 @@
-//
-//  main.cpp
-//  Dsa_CPP
-//
-//  Created by Polisetty Himeshwar on 25/3/26.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+void print1(int n){
+    for ( int i = 0; i < n ; i++){
+        for( int j = 0; j <= i ; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+void print2(int n){
+    for( int i =0 ; i< n; i++){
+        for(int j=0 ; j<=i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+void print3( int n){
+    for ( int i = 1; i<=n; i++){
+        for(int j=1 ; j<=i ; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void print4(int n){
+    for ( int i=1 ; i<=n; i++){
+        for(int j=1 ; j<=i ; j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void print5(int n){
+    for (int i = 1 ; i <=  n ; i++){
+        for (int j = 0 ; j< n-i+1 ; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+void print6(int n){
+    for ( int i = 1 ; i<= n ; i++){
+        for( int j =1 ; j<=n-i+1 ; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+void print7(int n){
+    for ( int i=0 ; i<=n ;i++){
+        //space
+        for(int j =0 ; j<(n-i+1);j++){
+            cout<<" ";
+        }
+        //star
+        for(int j = 0 ; j<(2*i+1);j++){
+            cout<<"*";
+        }
+        //space
+        for(int j =0 ; j<(n-i+1);j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+    void print8(int n) {
+        for(int i = 0 ; i < n ; i++){
+            for(int j =0 ; j < i;j++){
+                cout<<" ";
+            }
+            for(int j=0 ; j< 2*n -(2*i+1);j++){
+                cout<<"*";
+            }
+            for(int j =0 ; j< i ; j++){
+                cout<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+void print9(int n){
+    for ( int i=0 ; i<n ;i++){
+        //space
+        for(int j =0 ; j<(n-i+1);j++){
+            cout<<" ";
+        }
+        //star
+        for(int j = 0 ; j<(2*i+1);j++){
+            cout<<"*";
+        }
+        //space
+        for(int j =0 ; j<(n-i+1);j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i = 0 ; i < n ; i++){
+        for(int j =0 ; j < i;j++){
+            cout<<" ";
+        }
+        for(int j=0 ; j< 2*n -(2*i+1);j++){
+            cout<<"*";
+        }
+        for(int j =0 ; j< i ; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+void print10(int n){
+    for(int i = 1 ; i<= n ;i++){
+        for(int j = 0 ; j<i ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i = 1 ;i < n ;i++){
+        for ( int j = 0; j < n-i ; j++ ){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    print10(n);
+    
 }
