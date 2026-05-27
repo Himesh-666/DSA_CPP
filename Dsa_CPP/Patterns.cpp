99dbd680f8ca467e4824834ae9acd37af9bb359c
Himeshwar polisetty <himeshwarpolisetty@gmail.com>
2026-05-27 22:07:20 +0530

    Updated on 27/May/26

--- Dsa_CPP/Patterns.cpp
+++ Dsa_CPP/Patterns.cpp
@@ -134,10 +134,100 @@ void print10(int n){
     }
 }
 
+void print11(int n){
+    int start = 1 ;
+    for(int i = 0 ; i< n ;i++){
+        if(i%2==0) start = 1;
+        else start = 0;
+        for ( int j =0 ; j<= i ;j++){
+            cout<<start;
+            start = 1 -start;
+        }
+        cout<<endl;
+    }
+}
+
+void print12(int n){
+    for(int i = 1; i<= n ; i++){
+        for(int j =1  ; j<=i;j++){
+            cout<<j;
+        }
+        for(int j =0 ; j <2*n-2*i ; j++){
+            cout<< " ";
+        }
+        for(int j = i; j>0 ;j--){
+            cout<<j;
+      
+        }
+        cout<<endl;
+    }
+}
+
+void print13(int n){
+    int num = 1 ;
+    for(int i = 1 ; i<=n ; i++ ){
+        for(int j =0 ; j<i ; j++){
+            cout<<num<<" ";
+            num+=1;
+        }
+        cout<<endl;
+    }
+}
+
+void print14(int n){
+    for(int i = 0; i < n; i++){
+        for(char ch = 'A'; ch <= 'A' + i; ch++){
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+void print15(int n){
+
+    for(int i = 0; i < n; i++){
+
+        for(char ch = 'A'; ch <= (char)('A' + (n-i-1)); ch++){
+            cout << ch << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void print16(int n){
+    for ( int i =0 ;i<n;i++){
+        char ch = 'A';
+        for(int j= 0;j<=i;j++){
+            cout<<char(ch+i);
+        }
+        cout<<endl;
+    }
+}
+
+void print17(int n){
+    char ch='A';
+    for ( int i=0 ; i<=n ;i++){
+        //space
+        for(int j =0 ; j<(n-i+1);j++){
+            cout<<" ";
+        }
+        //char
+        for(int j = 0 ; j<(2*i+1);j++){
+            cout<<ch;
+
+        }
+        //space
+        for(int j =0 ; j<(n-i+1);j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
     int n;
     cin>>n;
-    print10(n);
+    print17(n);
     
 }
